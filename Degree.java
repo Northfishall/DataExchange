ca43d6e3e1cc5209cc33acfdd05278787d5400ad
fky <370136921@qq.com>
2019-10-13 15:23:11 +0800

    13

--- Degree.java
+++ Degree.java
@@ -1,28 +1,22 @@
 package com.foodSafe.Blockchain.control;
 
-import com.foodSafe.Blockchain.service.mongodbControl;
-import com.foodSafe.Blockchain.control.WorkOutData;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class AVGshortestPath {
-    @PostMapping("/AVGshortestPath")
+public class Degree {
+    @PostMapping("/Degree")
     @ResponseBody
-    public Map<String , String> GetAVGSP(@RequestBody Map<String,Object> JsData)
+    public Map<String , String> Degree(@RequestBody Map<String,Object> JsData)
     {
         WorkOutData V = new WorkOutData(""+JsData.get("Type"),""+JsData.get("Value"));
-        int [][] Dis= V.ShortestPath();
-        double length = V.AvgShortest(Dis);
-        System.out.println(length);
         Map<String , String> Result = new HashMap<>();
-        Result.put("Length",length+"");
+        Result.put("Result","success");
         return Result;
     }
 }
